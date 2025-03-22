6ed9daff303ca31362c5a187122f1da3b5e7ecd3
victor-herison-estacio <202503381755@alunos.estacio.br>
2025-03-22 16:16:12 +0000

    Desafio novato

--- xadrez.c
+++ xadrez.c
@@ -1,32 +1,103 @@
 #include <stdio.h>
 
-// Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
 int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+    int rainha = 8;
+    int torre = 7;
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    // determinando a posição do bispo como se fosse em um plano cartesiano
+    int  bispo_x = 2;
+    int bispo_y = 4;
+    int soma_x, soma_y = 0;
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    // movimentação do bispo;
+    while (bispo_x == 2 && bispo_y ==4)
+    {
+        printf("\n\n====== Movimentação do bispo! ======\n\n");
+        printf("A posição atual do bispo é (2,0)\n");
+        printf("Se vc escolher um numero positivo para x ele vai se movimentar para a direita, e o contrario caso negativo\n");
+        printf("Se vc escolher um numero positivo para y ele vai se movimentar para cima, e o contrario caso negativo\n");
+        printf("Escolha a movimentação do bispo (x,y) respectivamente: \n");
+        scanf("%d", &soma_x);
+        scanf("%d", &soma_y);
 
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+        //somando o numero setado pelo usuario + o numero da posição atual do bispo para saber em que casa o bispo esta
 
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
+        soma_x += bispo_x;
+        soma_y += bispo_y;
 
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
+        if (soma_x > 7 || soma_y > 7 || soma_x < 0 || soma_y < 0)
+        {
+            printf("O tabuleiro vai da posição 0 a 7(para x e y), não é possivel movimentar uma peça além disso!\n");
+            printf("A posição que o tabuleiro passou foi (%d,%d)\n", soma_x, soma_y);
+            printf("Corrija!\n");
 
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
+            //retornando ao valor original para o loop n acabar
+            soma_x, soma_y = 0;
+            bispo_x = 2;
+            bispo_y = 4;
+        }else{
+
+            printf("O Bispo se movimentou em x para:\n");
+            if (soma_x > bispo_x)
+            {
+                for (int i = 0; i < (soma_x - bispo_x); i++) // vai printar direita a quantidade de casas que a nova posição for maior que a casa antiga
+                {
+                    printf("Direita\n");
+                }
+                
+            }else if(soma_x == bispo_x){ //verifica se ele não andou
+               printf("Não andou");
+            }else{
+                for (int i = 0; i < (bispo_x - soma_x); i++)
+                {
+                    printf("esquerda\n");
+                }
+            }
+
+            //mesma verificação para y
+            printf("\n\nO Bispo se movimentou em y para:\n");
+            if (soma_y > bispo_y)
+            {
+                for (int i = 0; i < (soma_y - bispo_y); i++) // vai printar direita a quantidade de casas que a nova posição for maior que a casa antiga
+                {
+                    printf("Cima\n");
+                }
+                
+            }else if(soma_y == bispo_y){ //verifica se ele não andou
+               printf("Não andou");
+            }else{
+                for (int i = 0; i < (bispo_y - soma_y); i++)
+                {
+                    printf("Baixo\n");
+                }
+            }
+            
+            printf("\n\nA nova posição do bispo é: (%d, %d)", soma_x,soma_y);
+            break;
+        }
+    }
+    //vou fazer o resto do desafio sem acrescentar dificuldade
+    int count = 0;
+    printf("\n\n\nAa torre se movimentou em: \n");
+    do
+    {
+        printf("\ndireita");
+        count++;
+    } while (count < torre);
+
+    printf("\n\nA rainha se movimentou em: ");
+
+    for(int i = 0; i < rainha; i++){
+        printf("\nCima");
+    }
+
+
+    
+
+
+
+
 
     return 0;
 }
